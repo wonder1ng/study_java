4d08dc6a568b4ccb12f5d4041890f3cc30e59a8d
wonder1ng <wonder1ng@velog.io>
2024-03-27 09:20:25 +0900

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+// 클래스 블록
+public class HelloWorld {
+    /* 메서드 블록 */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+//    /* 메서드 블록 */
+//    [public|private|protected] [static] (리턴자료형|void) 메서드명2(입력자료형 매개변수, ...) {
+//        명령문(statement);
+//        ...
+//    }
+}
