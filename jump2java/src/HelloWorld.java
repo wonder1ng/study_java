513776155dbab3d296481d9e40a3bc167e6c1510
wonder1ng <wonder1ng@velog.io>
2024-03-27 09:37:40 +0900

    updated

--- jump2java/src/HelloWorld.java
+++ jump2java/src/HelloWorld.java
@@ -3,11 +3,51 @@ public class HelloWorld {
     /* 메서드 블록 */
     public static void main(String[] args) {
         System.out.println("Hello World");
-    }
+        int a = 2;
+        int b = 4;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("----------");
 
-//    /* 메서드 블록 */
-//    [public|private|protected] [static] (리턴자료형|void) 메서드명2(입력자료형 매개변수, ...) {
-//        명령문(statement);
-//        ...
-//    }
+        double a2 = 2;
+        double b2 = 4;
+        System.out.println(a2+b2);
+        System.out.println(a2-b2);
+        System.out.println(a2*b2);
+        System.out.println(a2/b2);
+        System.out.println(a2%b2);
+        System.out.println("----------");
+
+        int i =0;
+        int j = 10;
+        i++;
+        j--;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(j++);
+        System.out.println(j);
+        System.out.println(++j);
+        System.out.println(j);
+
+        boolean isSuccess = true;
+        boolean isTest = false;
+
+        int base = 100;
+        int height = 185;
+        boolean isTall = height < base;
+
+        if (isTall) {
+            System.out.println("키가 큽니다.");
+        } else {System.out.println("키가 작습니다.");
+
+        }
+        boolean isOdd = base % 99 == 1;
+        System.out.println(base % 99);
+        System.out.println(isOdd);
+    }
+    public static void main2(String[] args) {
+    }
 }
